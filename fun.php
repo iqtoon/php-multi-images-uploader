c3edb25ec655ea70206618619812f74dd6f1c065
Ahmed Almohamdi <virusalfaluja@gmail.com>
2023-01-10 03:23:23 +0300

    Create fun.php

---
+++ fun.php
@@ -0,0 +1,41 @@
+<?php
+
+if (isset($_FILES['images']['name'])){
+
+	$targetDir = "uploads/"; 
+
+	$allowTypes = array('jpg','png','jpeg','gif');
+
+	foreach($_FILES['images']['name'] as $key=>$val){ 
+
+	//file_put_contents('uploads/file_name.jpg', $value);
+
+		$imgfile = $_FILES['images']['name'][$key];
+
+		echo($img);
+
+		$fileName = basename($_FILES['images']['name'][$key]); 
+
+		$targetFilePath = $targetDir . $fileName; 
+
+		$fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION); 
+
+		$rand = rand(1, 1000000000);
+
+		$newname = $rand . date(Ymd);
+
+		if(in_array($fileType, $allowTypes)){     
+
+			$filename = $_FILES['images']['tmp_name'][$key];
+
+			if(move_uploaded_file($filename, $targetFilePath . $newname . '.' . $fileType)){ 
+
+				rename($targetFilePath . $newname . '.' . $fileType, $targetDir . $newname . '.' . $fileType); 
+			
+			} 
+		} 
+	}
+
+	echo json_encode('success');
+}
+?>
